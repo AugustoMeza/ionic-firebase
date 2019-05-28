bb3460729a9d24b5db4da5a6b0122274c1811fdc
Augusto Meza <j.aug.meza@gmail.com>
2019-05-28 01:41:33 -0600

    FireBase Project
    A simple todo app to test ionic + firebase

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskI} from '../models/task.interface';
+import {TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+
+  todos: TaskI[];
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(){
+    this.todoService.getTodos().subscribe( res => {
+      console.log('Tareas', res);
+      this.todos = res;
+    });
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA-qpFvZGh0O2Q56sAKLLj7kfOaoU3IFhY",
+    authDomain: "crud-ionic-meza.firebaseapp.com",
+    databaseURL: "https://crud-ionic-meza.firebaseio.com",
+    projectId: "crud-ionic-meza",
+    storageBucket: "crud-ionic-meza.appspot.com",
+    messagingSenderId: "858665170156",
+    appId: "1:858665170156:web:e7f64a2d95f5c097"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
